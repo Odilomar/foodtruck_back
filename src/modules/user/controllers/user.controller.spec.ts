6357152aa308e73d813d733ca27a9bf042e6a670
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-08 18:55:08 -0300

    feat: update user controller

--- src/modules/user/controllers/user.controller.spec.ts
+++ src/modules/user/controllers/user.controller.spec.ts
@@ -1,4 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserUseCase } from '../use-cases/create-user.use-case';
+import { DeleteUserUseCase } from '../use-cases/delete-user.use-case';
+import { FindOneUserUseCase } from '../use-cases/find-one-user.use-case';
+import { FindUserUseCase } from '../use-cases/find-user.use-case';
+import { UpdateUserUseCase } from '../use-cases/update-user.use-case';
 import { UserController } from './user.controller';
 
 describe('UserController', () => {
@@ -6,6 +11,38 @@ describe('UserController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CreateUserUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: DeleteUserUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: FindOneUserUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: FindUserUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: UpdateUserUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
       controllers: [UserController],
     }).compile();
 
