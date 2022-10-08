07d3dee8d52059364b5f8ad61f2baac8b3871681
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-08 18:43:23 -0300

    chore: add bcrypt

--- src/modules/user/use-cases/create-user.use-case.ts
+++ src/modules/user/use-cases/create-user.use-case.ts
@@ -3,11 +3,20 @@ import { UserEntity } from '../domain/entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from '../infra/repositories/user.repository';
 
+import * as bcrypt from 'bcrypt';
+import configuration from '@/config/configuration';
+
 @Injectable()
 export class CreateUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
   async execute(input: CreateUserDto) {
     const user = new UserEntity(input);
+
+    user.password = await bcrypt.hash(
+      user.password,
+      configuration().auth.saltRounds,
+    );
+
     return this.userRepository.save(user);
   }
 }
