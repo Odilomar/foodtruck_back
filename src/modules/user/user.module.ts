7b9373d2f7d703b288e01a9e75c11bd451917e55
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-08 18:16:28 -0300

    feat: import use cases to user

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './infra/model/user.model';
 import { UserRepository } from './infra/repositories/user.repository';
 import { CreateUserUseCase } from './use-cases/create-user.use-case';
+import { DeleteUserUseCase } from './use-cases/delete-user.use-case';
+import { FindOneUserUseCase } from './use-cases/find-one-user.use-case';
+import { FindUserUseCase } from './use-cases/find-user.use-case';
 import { UpdateUserUseCase } from './use-cases/update-user.use-case';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserModel])],
-  providers: [UserRepository, CreateUserUseCase, UpdateUserUseCase],
+  providers: [
+    UserRepository,
+    CreateUserUseCase,
+    UpdateUserUseCase,
+    DeleteUserUseCase,
+    FindUserUseCase,
+    FindOneUserUseCase,
+  ],
 })
 export class UserModule {}
