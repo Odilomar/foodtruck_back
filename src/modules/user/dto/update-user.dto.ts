69e59cbc0c798d8016ae50469dea59cfb2bb7ddb
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-08 18:23:35 -0300

    feat: add swagger for dtos

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,21 +1,27 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsNumber()
   id: number;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   email?: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   password?: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   cpf?: string;
