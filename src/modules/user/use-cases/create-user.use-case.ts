2524617c5cdceeda6e023e92c2a312aabf1e7e87
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-08 18:50:17 -0300

    feat: add cpf validation for create user

--- src/modules/user/use-cases/create-user.use-case.ts
+++ src/modules/user/use-cases/create-user.use-case.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserEntity } from '../domain/entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from '../infra/repositories/user.repository';
 
 import * as bcrypt from 'bcrypt';
 import configuration from '@/config/configuration';
+import { cpf } from 'cpf-cnpj-validator';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
   async execute(input: CreateUserDto) {
     const user = new UserEntity(input);
+
+    if (!cpf.isValid(user.cpf)) {
+      throw new BadRequestException('CPF is not valid!');
+    }
 
     user.password = await bcrypt.hash(
       user.password,
