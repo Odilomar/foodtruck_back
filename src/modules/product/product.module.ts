11ccbc3e340e9c45a41a40188847cad6ade3018c
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-09 14:20:15 -0300

    feat: export product repository and model

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModel } from './infra/model/product.model';
+import { ProductRepository } from './infra/repositories/product.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductModel])],
+  providers: [ProductRepository],
+})
 export class ProductModule {}
