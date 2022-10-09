74e6930827087ab961c8a65b43ba3d1a7b30f9c1
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-09 14:42:15 -0300

    feat: export use cases and controller

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductController } from './controllers/product.controller';
 import { ProductModel } from './infra/model/product.model';
 import { ProductRepository } from './infra/repositories/product.repository';
+import { CreateProductUseCase } from './use-cases/create-product.use-case';
+import { DeleteProductUseCase } from './use-cases/delete-product.use-case';
+import { FindOneProductUseCase } from './use-cases/find-one-product.use-case';
+import { FindProductUseCase } from './use-cases/find-product.use-case';
+import { UpdateProductUseCase } from './use-cases/update-product.use-case';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductModel])],
-  providers: [ProductRepository],
+  providers: [
+    ProductRepository,
+    CreateProductUseCase,
+    UpdateProductUseCase,
+    FindOneProductUseCase,
+    FindProductUseCase,
+    DeleteProductUseCase,
+  ],
+  controllers: [ProductController],
 })
 export class ProductModule {}
