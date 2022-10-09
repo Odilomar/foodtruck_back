18726bfe7141c6d9e9852b9b0a52ca5dd5491bcb
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-09 14:37:19 -0300

    feat: find product use case with unit test

---
+++ src/modules/product/use-cases/find-product.use-case.spec.ts
@@ -0,0 +1,54 @@
+import { ProductTypeEnum } from '@/modules/shared/enum/product-type.enum';
+import { faker } from '@faker-js/faker';
+import { Test, TestingModule } from '@nestjs/testing';
+import { FindProductDto } from '../dto/find-product.dto';
+import { ProductModel } from '../infra/model/product.model';
+import { ProductRepository } from '../infra/repositories/product.repository';
+import { FindProductUseCase } from './find-product.use-case';
+
+describe('FindProductUseCase', () => {
+  let service: FindProductUseCase;
+
+  const input = {
+    search: faker.datatype.string(),
+  } as FindProductDto;
+
+  const product = {
+    id: faker.datatype.number(),
+    title: faker.datatype.string(),
+    description: faker.datatype.string(),
+    url: faker.datatype.string(),
+    unit_price: faker.datatype.number(),
+    type: ProductTypeEnum.DRINK,
+    created_by_id: faker.datatype.number(),
+    created_at: faker.date.past(),
+    updated_at: faker.date.future(),
+  } as ProductModel;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FindProductUseCase,
+        {
+          provide: ProductRepository,
+          useValue: {
+            find: jest.fn().mockReturnValue([product]),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<FindProductUseCase>(FindProductUseCase);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should find products', async () => {
+    const founded = await service.execute(input);
+
+    expect(founded.length).toBeGreaterThan(0);
+    expect(founded[0]).toHaveProperty('id', product.id);
+  });
+});
---
+++ src/modules/product/use-cases/find-product.use-case.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { FindOptionsWhere, Like } from 'typeorm';
+import { FindProductDto } from '../dto/find-product.dto';
+import { ProductModel } from '../infra/model/product.model';
+import { ProductRepository } from '../infra/repositories/product.repository';
+
+@Injectable()
+export class FindProductUseCase {
+  constructor(private readonly productRepository: ProductRepository) {}
+  async execute({ search }: FindProductDto) {
+    const where: FindOptionsWhere<ProductModel>[] = [];
+
+    if (search) {
+      where.push({
+        title: Like(search),
+      });
+      where.push({
+        description: Like(search),
+      });
+    }
+
+    return this.productRepository.find(where.length > 0 ? { where } : {});
+  }
+}
