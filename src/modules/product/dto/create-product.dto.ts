b6cfae0bd671d62f5a8edf8f7e4139e3442b018a
Odilomar Júnior <odilomar.junior@gmail.com>
2022-10-09 14:24:39 -0300

    feat: create product dtos

---
+++ src/modules/product/dto/create-product.dto.ts
@@ -0,0 +1,33 @@
+import { ProductTypeEnum } from '@/modules/shared/enum/product-type.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  url: string;
+
+  @ApiProperty()
+  @IsNumber()
+  unit_price: number;
+
+  @ApiProperty()
+  @IsEnum(ProductTypeEnum)
+  type: ProductTypeEnum;
+
+  @ApiProperty()
+  @IsNumber()
+  created_by_id: number;
+
+  @ApiProperty()
+  @IsNumber()
+  updated_by_id?: number;
+}
---
+++ src/modules/product/dto/update-product.dto.ts
@@ -0,0 +1,44 @@
+import { ProductTypeEnum } from '@/modules/shared/enum/product-type.enum';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateProductDto {
+  @ApiProperty()
+  @IsNumber()
+  id: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  url?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  unit_price?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsEnum(ProductTypeEnum)
+  type?: ProductTypeEnum;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  created_by_id?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  updated_by_id?: number;
+}
