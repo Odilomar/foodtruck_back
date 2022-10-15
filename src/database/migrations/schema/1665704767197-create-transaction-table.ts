90664f1eaf2b7515a3abbb70ee5c376acd0f3e38
Odilomar Júnior <BRSSI0007@totvspartners.com.br>
2022-10-15 12:24:06 -0300

    feat: create transaction migration

---
+++ src/database/migrations/schema/1665704767197-create-transaction-table.ts
@@ -0,0 +1,65 @@
+import { PaymentTypeEnum } from '@/shared/enum/payment-type.enum';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTransactionTable1665704767197 implements MigrationInterface {
+  private readonly tableName = 'transactions';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'serial',
+            generationStrategy: 'increment',
+            isPrimary: true,
+          },
+          {
+            name: 'user_id',
+            type: 'integer',
+          },
+          {
+            name: 'customer_cpf',
+            type: 'varchar',
+            length: '15',
+          },
+          {
+            name: 'net_total',
+            type: 'integer',
+          },
+          {
+            name: 'discounts',
+            type: 'integer',
+          },
+          {
+            name: 'gross_total',
+            type: 'integer',
+          },
+          {
+            name: 'payment_type',
+            type: 'varchar',
+            default: `'${PaymentTypeEnum.CASH}'`,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp with time zone',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
